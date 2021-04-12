31715a757a8435769b4f1c5450414cfe2fd20f52
maxiluckies <luckiesmaxi6@gmail.com>
2021-04-13 04:59:29 +0700

    add manual

--- app/imsakiyah_app.py
+++ app/imsakiyah_app.py
@@ -10,6 +10,16 @@ parser.add_argument('long')
 parser.add_argument('start')
 parser.add_argument('days')
 
+@app.route('/')
+def manual():
+    return "<h2> Cara Penggunaan </h2> </br>" \
+           "https://imsakiyah.herokuapp.com/imsakiyah/&lt;lat>/&lt;lon>/&lt;startdate>/&lt;days> </br>" \
+           "contoh : <a href='https://imsakiyah.herokuapp.com/imsakiyah/-7.797068/110.370529/2021-04-13/30'>https://imsakiyah.herokuapp.com/imsakiyah/-7.797068/110.370529/2021-04-13/30</a>" \
+           "<h2>Import ke Calendar</h2> </br>" \
+           "<p>Setelah Anda menjalankan perintah di atas, sebuah file <b>ics</b> akan terdownload." \
+           "Silakan buka apps <b>Calendar</b> di OSX, klik <b>File > Import</b> lalu pilih file hasil download."
+
+
 @app.route('/imsakiyah/<string:lat>/<string:lon>/<string:date>/<int:days>')
 def imsakiyah(lat,lon,date,days):
 
